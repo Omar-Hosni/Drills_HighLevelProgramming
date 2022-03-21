e03c71bf057af609ca8d6d1a0b84372926093454
Omar Hosny <68732608+Omar-Hosni@users.noreply.github.com>
2022-03-21 12:56:03 +0100

    Create Chapter8Drills.cpp

---
+++ Chapter8Drills.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+template<typename T>
+void swap_v(T a, T b)
+{
+  int temp = a;
+  a = b;
+  b = temp;
+}
+
+template<typename T>
+void swap_r(T &a, T &b)
+{
+ int temp = a;
+  a = b;
+  b = temp;
+}
+
+template<typename T>
+void swap_cr(const T& a, const T& b)
+{
+ int temp = a;
+  a = b;
+  b = temp;
+}
+
+int main()
+{
+  int x = 7;
+  int y = 9;
+  swap_v(x,y);
+  swap_r(x,y);
+  
+  const int cx = 7;
+  const int cy = 9;
+  
+  swap_cr(cx,cy);
+  swap_v(7.7,9.9);
+  
+  double dx = 7.7;
+  double dy = 9.9;
+  swap_r(dx,dy);
+  
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
