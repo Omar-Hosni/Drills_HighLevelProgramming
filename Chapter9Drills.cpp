4fdba477d9f0efbdcffdac129eab8106643d7427
Omar Hosny <68732608+Omar-Hosni@users.noreply.github.com>
2022-03-23 14:33:02 +0300

    Update Chapter9Drills.cpp

--- Chapter9Drills.cpp
+++ Chapter9Drills.cpp
@@ -1,64 +1,104 @@
 #include <iostream>
-#include <string>
 #include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <utility>
 
 using namespace std;
 
 class Name_pairs
 {
- private:
-  string name;
-  double age;
-  int size;
-  vector<string> names;
-  vector<double> ages;
-  
 public:
-  void read_names()
-  {
-   int s;
-    cout<<"how many names do you want to input: ";
-    cin>>s;
-    this->size = s;
-    names(s);
-    
-    for(int i =0; i < size; i++)
-    {
-      cout<<"input name: "<<i<<": ";
-      cin>>names[i];
-    }
-  }
-  
-  void read_ages()
-  {
-   cout<<"you have "<<size<<" names so you will input "<<size<<" ages for each one\n";
-   for(int i =0; i < size; i++)
-   {
-    cout<<names[i]<<" age: ";
-    cin>>ages[i];
-   }
-  }
-  
-  void print()
-  {
-   for(int i  =0; i < size; i++)
-   {
-    cout<<names[i]<<" "<<ages[i]<<endl; 
-   }
-  }
-  
-}
+	vector<string> names;
+	vector<double> ages;
 
+	void read_names()
+	{
+		int size;
+		cout << "how many names you want to input: ";
+		cin >> size;
+
+		names.resize(size);
+
+		for (int i = 0; i < size; i++)
+		{
+			cout << "enter name " << i + 1 << ": ";
+			cin >> names[i];
+		}
+	}
+
+	vector<string> get_names()
+	{
+		return names;
+	}
+
+	int get_names_size()
+	{
+		return names.size();
+	}
+
+	void read_ages()
+	{
+		int size = get_names_size();
+		vector<string> n = get_names();
+
+		ages.resize(size);
+
+		cout << "you have " << size << " names, you'll enter age for each\n";
+
+		for (int i = 0; i < size; i++)
+		{
+			cout << n[i] << " age: ";
+			cin >> ages[i];
+		}
+	}
+
+	vector<double> get_ages()
+	{
+		return ages;
+	}
+
+	void pair_and_sort_and_print()
+	{
+		std::map<string, int> myMap;
+		
+		//assigning key-value pairs in a map
+		for (int i = 0; i < names.size(); i++)
+		{
+			myMap[names[i]] = ages[i];
+
+		}
+
+		//sorting a map
+		vector<std::pair<string, int>> v;
+
+		for (auto& it : v)
+		{
+			v.push_back(it);
+		}
+
+		sort(v.begin(), v.end());
+
+		//printing values
+		auto it = myMap.begin();
+		while (it != myMap.end())
+		{
+			cout << it->first << " " << it->second << endl;
+				it++;
+		}
+	}
+
+
+};
 
 int main()
 {
- 
-  Name_pairs np;
-  np.read_names();
-  np.read_ages();
-  np.print();
-  
-  return 0;
+	Name_pairs np;
+	np.read_names();
+	np.read_ages();
+	np.pair_and_sort_and_print();
+
 }
 
 
